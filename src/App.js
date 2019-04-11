0559be3031442833791992ccf1cbb7dd24485c83
Steven Clouston <stevenclouston@gmail.com>
2019-04-12 01:36:00 +1200

    Add message to show the result of the game.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './assets/css/App.css';
 import Board from "./components/board";
 import Title from "./components/title";
 import StartPage from "./components/startPage";
+import GameStatus from "./components/gameStatus";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-body">
+            <div className="status">
+                <GameStatus/>
+            </div>
             <Title/>
             <StartPage/>
-            <Board />
+            <Board/>
         </header>
       </div>
     );
---
+++ src/components/gameStatus.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import '../assets/css/board.css';
+import connect from "react-redux/es/connect/connect";
+import Fade from "@material-ui/core/Fade";
+
+const mapStateToProps = (state, ownProps) => ({
+    gameContext: state.gameContext,
+    winner: state.gameContext.winner,
+    draw: state.gameContext.draw,
+});
+
+class GameStatus extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        const status = () => {
+            if (this.props.draw) {
+                return "It's a draw!"
+            }
+            if (this.props.winner === this.props.gameContext.usersTeam){
+                return "You won!"
+            } else {
+                return "Better luck next time!"
+            }
+
+        }
+
+        const showDisplay = () => {
+            if (this.props.winner || this.props.draw) {
+                return true
+            }
+
+            return false
+        }
+
+        const message = (
+            <Fade  {...{timeout: {enter: 1000, exit: 0} }} in={showDisplay()} mountOnEnter unmountOnExit>
+                <div style={{position: "relative"}}>
+                    <div style={{position: "relative", width:"100%"}}>
+                        <h4>
+                            {status()}
+                        </h4>
+                    </div>
+                </div>
+            </Fade>
+        )
+
+        return (
+            message
+        )
+    }
+}
+
+export default connect(mapStateToProps, null)(GameStatus);
