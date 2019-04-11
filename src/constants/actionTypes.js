6dad48192893b39f74431eec618a9c4d58e8da9d
Steven Clouston <stevenclouston@gmail.com>
2019-04-12 00:05:47 +1200

    Update action types to for new reducers.

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,1 +1,5 @@
 export const RESET_GAME = 'RESET_GAME';
+export const UPDATE_BOX = 'UPDATE_BOX';
+export const UPDATE_BOX_ASYNC = 'UPDATE_BOX_ASYNC'
+export const CREATE_BOARD = 'CREATE_BOARD';
+export const CREATE_BOX = 'CREATE_BOX';
