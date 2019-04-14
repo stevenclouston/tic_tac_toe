f032345545da0f3d91c6cab84fcd812deb6cccd4
Steven Clouston <stevenclouston@gmail.com>
2019-04-14 21:53:45 +1200

    Add test to test CREATE_BOX action of box reducer.

---
+++ src/reducers/tests/box.test.js
@@ -0,0 +1,17 @@
+import reducer from "../box";
+import { CREATE_BOX } from "../../constants/ActionTypes";
+
+describe("actions", () => {
+  it("should create a box", () => {
+    const action = {
+      type: CREATE_BOX,
+      id: 1
+    };
+    const expectedAction = {
+      1: {
+        value: null
+      }
+    };
+    expect(reducer({}, action)).toEqual(expectedAction);
+  });
+});
